4d7e4dea53a347a812b5171d7699ef5db7471242
Tobias Lindberg (Dell) <tobias.lindberg@sodertalje.ntig.se>
2019-02-04 21:48:23 +0100

    refakturerat så att Lesson-klassen ärver av Label-klassen.

--- t-schema/Lesson.cs
+++ t-schema/Lesson.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace t_schema
 {   
-    class Lesson
+    class Lesson:Label
     {
         DayOfWeek weekDay;
         DateTime startTime;
@@ -16,26 +16,71 @@ namespace t_schema
         string course;
         string teacher;
         string room;
-        Color color;
 
         public Lesson(DayOfWeek weekDay, DateTime startTime, int length, string course, 
             string teacher, string room, Color color)
         {
-            this.WeekDay = weekDay;
-            this.StartTime = startTime;
-            this.Length = length;
-            this.Course = course;
-            this.Teacher = teacher;
-            this.Room = room;
-            this.Color = color;
+            this.weekDay = weekDay;
+            this.startTime = startTime;
+            this.length = length;
+            this.course = course;
+            this.teacher = teacher;
+            this.room = room;
+
+            this.Location = new System.Drawing.Point(35 + DayNumber() * 100, 6 + ((startTime.Hour - 8) * 60 + startTime.Minute) / 10 * 6);
+            //this.Name = "lblLesson" + lessonNo;
+            this.AutoSize = false;
+            this.BackColor = color;
+            this.Size = new System.Drawing.Size(90, length / 10 * 6);
+            //this.TabIndex = 9 + lessonNo;
+            this.Text = Time(startTime) + "-" + Time(EndTime()) + "\n"
+                        + room + "  " + course + "\n"
+                        + teacher;
         }
 
-        public DayOfWeek WeekDay { get => weekDay; set => weekDay = value; }
-        public DateTime StartTime { get => startTime; set => startTime = value; }
-        public int Length { get => length; set => length = value; }
-        public string Course { get => course; set => course = value; }
-        public string Teacher { get => teacher; set => teacher = value; }
-        public string Room { get => room; set => room = value; }
-        public Color Color { get => color; set => color = value; }
+        //Omvandlar ett värde av typen DateTime till en string
+        private string Time(DateTime time)
+        {
+            string strTime, extra;
+
+            if (time.Minute < 10)
+                extra = "0";
+            else
+                extra = "";
+
+            strTime = time.Hour + ":" + extra + time.Minute;
+
+            return strTime;
+        }
+
+        //Retrunerar sluttiden
+        private DateTime EndTime()
+        {
+            return startTime + TimeSpan.FromMinutes(length);
+        }
+
+        //Tar reda på vilket nummer en veckodag har (0-6)
+        private int DayNumber()
+        {
+            switch (weekDay)
+            {
+                case DayOfWeek.Sunday:
+                    return 6;
+                case DayOfWeek.Monday:
+                    return 0;
+                case DayOfWeek.Tuesday:
+                    return 1;
+                case DayOfWeek.Wednesday:
+                    return 2;
+                case DayOfWeek.Thursday:
+                    return 3;
+                case DayOfWeek.Friday:
+                    return 4;
+                case DayOfWeek.Saturday:
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
     }
 }
--- t-schema/Schedule.cs
+++ t-schema/Schedule.cs
@@ -13,7 +13,7 @@ namespace t_schema
     public partial class Schedule : Form
     {
 
-        List<Lesson> lessons = new List<Lesson>();
+        //List<Lesson> lessons = new List<Lesson>();
         Lesson tmpLesson;
         Label tmpLabel;
 
@@ -38,84 +38,22 @@ namespace t_schema
             //Läs in några exempel-lektioner. Bör bytas ut mot inläsning från databas
             tmpLesson = new Lesson(DayOfWeek.Tuesday, DateTime.Parse("12:40"), 80,
                 "Prg 2", "Tobias Lindberg", "Sal 1", Color.Aqua);
-            lessons.Add(tmpLesson);
+            this.Controls.Add(tmpLesson);
             tmpLesson = new Lesson(DayOfWeek.Wednesday, DateTime.Parse("9:55"), 80,
                 "Prg 2", "Tobias Lindberg", "Sal 1", Color.Aqua);
-            lessons.Add(tmpLesson);
+            this.Controls.Add(tmpLesson);
             tmpLesson = new Lesson(DayOfWeek.Thursday, DateTime.Parse("12:10"), 80,
                 "Prg 2", "Tobias Lindberg", "Sal 1", Color.Aqua);
-            lessons.Add(tmpLesson);
+            this.Controls.Add(tmpLesson);
             tmpLesson = new Lesson(DayOfWeek.Thursday, DateTime.Parse("8:25"), 80,
                 "Ma 5", "Hiba Jameel", "Sal 3", Color.GreenYellow);
-            lessons.Add(tmpLesson);
+            this.Controls.Add(tmpLesson);
             tmpLesson = new Lesson(DayOfWeek.Thursday, DateTime.Parse("9:55"), 80,
                 "Ma 5", "Hiba Jameel", "Sal 3", Color.GreenYellow);
-            lessons.Add(tmpLesson);
-
-            //lessonNo används 
-            int lessonNo = 0;
-            foreach (Lesson l in lessons)
-            {
-                lessonNo++;
-                //Omvandla lektionslängden till en sluttid för utskrift.
-                DateTime endTime = l.StartTime + TimeSpan.FromMinutes(l.Length);
-                //Skapa en label
-                tmpLabel = new Label
-                {
-                    Location = new System.Drawing.Point(35 + DayNumber(l.WeekDay) * 100, 6+((l.StartTime.Hour-8) * 60 + l.StartTime.Minute)/10*6),
-                    Name = "lblLesson" + lessonNo,
-                    AutoSize = false,
-                    BackColor = l.Color,
-                    Size = new System.Drawing.Size(90, l.Length/10*6),
-                    TabIndex = 9 + lessonNo,
-                    Text = Time(l.StartTime) + "-" + Time(endTime) + "\n" 
-                        + l.Room + "  " + l.Course + "\n"
-                        + l.Teacher
-                };
-                this.Controls.Add(tmpLabel);
-
-            }
-
+            this.Controls.Add(tmpLesson);
         }
 
-        //Omvandlar ett värde av typen DateTime till en string
-        private string Time(DateTime time)
-        {
-            string strTime, extra;
-
-            if (time.Minute < 10)
-                extra = "0";
-            else
-                extra = "";
-
-            strTime = time.Hour + ":" + extra + time.Minute;
-
-            return strTime;
-        }
-
-        //Tar reda på vilket nummer en veckodag har (0-6)
-        private int DayNumber(DayOfWeek dayOfWeek)
-        {
-            switch (dayOfWeek)
-            {
-                case DayOfWeek.Sunday:
-                    return 6;
-                case DayOfWeek.Monday:
-                    return 0;
-                case DayOfWeek.Tuesday:
-                    return 1; 
-                case DayOfWeek.Wednesday:
-                    return 2;
-                case DayOfWeek.Thursday:
-                    return 3;
-                case DayOfWeek.Friday:
-                    return 4;
-                case DayOfWeek.Saturday:
-                    return 5;
-                default:
-                    return 0;
-            }
-        }
+        
 
         private void lblLesson_Click(object sender, EventArgs e)
         {
